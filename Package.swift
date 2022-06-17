349731086c0a7fbdcbb769b4ccbdc116a11d114b
Devida Aurel <devida.aurelia@nexsoft.co.id>
2022-06-17 10:04:10 +0700

    chgs

--- Package.swift
+++ Package.swift
@@ -4,12 +4,12 @@
 import PackageDescription
 
 let package = Package(
-    name: "ReusableUI",
+    name: "ReusableUIAurel",
     platforms: [.iOS(.v13)],
     products: [
         // Products define the executables and libraries a package produces, and make them visible to other packages.
         .library(
-            name: "ReusableUI",
+            name: "ReusableUIAurel",
             targets: ["ReusableUI"]),
     ],
     dependencies: [
